ca34dbf25ebed2ae6b94ba7d10ab64ed6e534900
Adrean <44714401+Addaku@users.noreply.github.com>
2018-12-31 13:10:48 -0800

    Add files via upload

---
+++ OnMethods/OnMethods/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnMethods
+{
+    public class Math
+    {
+        public static int Math1(int getNum)
+        {
+            int addNum = 10 + getNum;
+            return addNum;
+        }
+        public static int Math2(int getNum)
+        {
+            int divideNum = getNum / 10;
+            return divideNum;
+        }
+        public static int Math3(int getNum)
+        {
+            int multiplyNum = getNum * 10;
+            return multiplyNum;
+        }
+    }
+}
---
+++ OnMethods/OnMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to math on.");
+            int getNum = Convert.ToInt32(Console.ReadLine());
+            int drawNum = 0;
+
+            drawNum = Math.Math1(getNum);
+            Console.WriteLine(drawNum);
+
+            drawNum = Math.Math2(getNum);
+            Console.WriteLine(drawNum);
+
+            drawNum = Math.Math3(getNum);
+            Console.WriteLine(drawNum);
+
+            Console.ReadLine();
+        }
+    }
+}
