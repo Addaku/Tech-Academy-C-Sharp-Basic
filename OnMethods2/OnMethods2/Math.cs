68ffe56c88d2afaba481269bf8815ec2ba246453
Adrean <44714401+Addaku@users.noreply.github.com>
2018-12-31 14:39:41 -0800

    Add files via upload

---
+++ OnMethods2/OnMethods2/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnMethods2
+{
+    class Mathy
+    {
+        public static void Math1 (int Num1, int Num2)
+        {
+            Num1 += 7;
+            Console.WriteLine(Num2);
+        }
+    }
+}
---
+++ OnMethods2/OnMethods2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathy mathy = new Mathy();
+
+            Mathy.Math1(1,2);
+            Mathy.Math1(Num2: 30, Num1: 4);
+            Console.ReadLine();
+        }
+    }
+}
