333a03bad0504ea663ca20608436378960047a55
Adrean <44714401+Addaku@users.noreply.github.com>
2018-12-31 16:18:26 -0800

    Add files via upload

---
+++ OnMethods3/OnMethods3/Mathy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnMethods3
+{
+    public class Mathy
+    {
+        public int Math1(int num1)
+        {
+            num1 += 7;
+            return num1;
+        }
+    
+        public float Math1(float num1)
+        {
+            num1 += 5.5f;
+            return num1;
+        }
+        public string Math1(string num1)
+        {
+            int num2 = Convert.ToInt32(num1);
+            num2 -= 7;
+            string num3 = Convert.ToString(num2);
+            return num3;
+        }
+    }
+}
---
+++ OnMethods3/OnMethods3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnMethods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathy mathy = new Mathy();
+            // method 1
+            Console.WriteLine(mathy.Math1(12));
+            //method 2
+            Console.WriteLine(mathy.Math1(12.5f));
+            // method3
+            Console.Write("Please enter a number for math: ");
+            Console.WriteLine(mathy.Math1(Console.ReadLine()));
+
+            
+            Console.ReadLine();
+        }
+    }
+}
