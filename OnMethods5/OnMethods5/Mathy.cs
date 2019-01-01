3d39d27ce095d28d39191d2b1e15b6487434957b
Adrean <44714401+Addaku@users.noreply.github.com>
2019-01-01 14:40:55 -0800

    Add files via upload

---
+++ OnMethods5/OnMethods5/Mathy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnMethods5
+{
+    public class Mathy
+    {
+        public void Half (int num)
+        {
+            int num1 = num / 2;
+            Console.WriteLine("{0} divided by 2 = {1}", num, num1);
+        }
+
+        public void Doubled(int num, out int randNum)
+        {
+            randNum = 6;
+            int num1 = num * 2;
+            Console.WriteLine("{0} multiplied by 2 = {1}", num, num1);
+        }
+
+        public void Half(int num, int num2)
+        {
+            int num1 = num / 2;
+            int num3 = num2 / 2;
+            Console.WriteLine("{0} divided by 2 = {1}", num, num1);
+            Console.WriteLine("{0} divided by 2 = {1}", num2, num3);
+        }
+    }
+}
---
+++ OnMethods5/OnMethods5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnMethods5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathy mathy = new Mathy();
+            mathy.Half(8);
+
+            Console.Write("Enter a number: ");
+            int getNum = Convert.ToInt32(Console.ReadLine());
+            mathy.Half(getNum);
+
+            mathy.Doubled(2, out int randNum);
+            Console.WriteLine("Your randnum was: " + randNum);
+
+            mathy.Half(12, 16);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
