b9c96e9d59b60c9cc5da3f77d20d68574a344d7b
Adrean <44714401+Addaku@users.noreply.github.com>
2019-01-03 12:47:22 -0800

    Add files via upload

---
+++ Interfaces/AbstractClasses/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    public class Employee : Person, IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("You Quit");
+        }
+    }
+}
---
+++ Interfaces/AbstractClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+
+            };
+            person.Sayname();
+
+            IQuittable quit = new Employee();
+            quit.Quit();
+            
+            Console.ReadLine();
+        }
+    }
+}
