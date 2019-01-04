f3708e2b00efd6288df60c6f8a460ebbf9bcdbed
Adrean <44714401+Addaku@users.noreply.github.com>
2019-01-04 13:44:09 -0800

    Add files via upload

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Employee<T> : Person, IQuittable
+    {
+
+        public int Id { get; set; }
+        public List<T> Things { get; set; }
+
+        public void Quit()
+        {
+            Console.WriteLine("You Quit");
+        }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+
+        public static bool operator ==(Employee<T> employee1, Employee<T> employee2)
+        {
+            bool dupe = (employee1.Id == employee2.Id);
+            return dupe;
+        }
+
+        public static bool operator !=(Employee<T> employee1, Employee<T> employee2)
+        {
+            bool dupe = (employee1.Id != employee2.Id);
+            return dupe;
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> strings = new Employee<string>();
+            strings.Things = new List<string>() { "one", "two", "three" };
+
+            Employee<int> intergers = new Employee<int>();
+            intergers.Things = new List<int>() { 1, 2, 3 };
+
+            foreach (string thing in strings.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (int thing in intergers.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Employee<string> person1 = new Employee<string>()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            person1.SayName();
+            IQuittable quit = new Employee<string>();
+            quit.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
