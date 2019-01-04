9ca3184c6cbbfb37930af935fc1c48ea9f3cf32a
Adrean <44714401+Addaku@users.noreply.github.com>
2019-01-04 12:56:36 -0800

    Add files via upload

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> strings = new Employee<string>
+            {
+                Things = new List<string>() { "one", "two", "three" }
+            };
+
+            Employee<int> intergers = new Employee<int>
+            {
+                Things = new List<int>() { 1, 2, 3 }
+            };
+
+            foreach (string thing in strings.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (int thing in intergers.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Employee<string> person1 = new Employee<string>()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+
+
+            person1.SayName();
+            IQuittable quit = new Employee<string>();
+            quit.Quit();
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
