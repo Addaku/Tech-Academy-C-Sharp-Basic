73e038f731a5b67b5382d242ee88470e5d2d0747
Adrean <44714401+Addaku@users.noreply.github.com>
2019-01-04 13:26:53 -0800

    Add files via upload

---
+++ OverloadingOperators/OverloadingOperators/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    public class Employee : Person, IQuittable
+    {
+
+        public int Id { get; set; }
+
+        public void Quit()
+        {
+            Console.WriteLine("You Quit");
+        }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            bool dupe = (employee1.Id == employee2.Id);
+            return dupe;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool dupe = (employee1.Id != employee2.Id);
+            return dupe;
+        }
+    }
+}
---
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee person1 = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            person1.SayName();
+            IQuittable quit = new Employee();
+            quit.Quit();
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
