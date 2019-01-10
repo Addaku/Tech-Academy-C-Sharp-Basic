6822fefdd482f8b15d443999ebc7bae229ef37e4
Adrean <44714401+Addaku@users.noreply.github.com>
2019-01-09 16:17:35 -0800

    Add files via upload

---
+++ IO/IO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string getNum = Console.ReadLine();
+            File.WriteAllText(@"C:\\Users\\Addak\\Test\\Test.txt", getNum);
+            // way 1
+            string textNum = File.ReadAllText(@"C:\\Users\\Addak\\Test\\Test.txt");
+            Console.WriteLine("Your number was: " + textNum);
+            // way 2
+            Console.WriteLine(File.ReadAllText(@"C:\\Users\\Addak\\Test\\Test.txt"));
+
+            Console.ReadLine();
+        }
+    }
+}
