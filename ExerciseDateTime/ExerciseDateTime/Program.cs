9c1ee6bc35d98f2b7c5af3eba9bb8966f4960f8c
Adrean <44714401+Addaku@users.noreply.github.com>
2019-01-09 16:55:24 -0800

    Add files via upload

---
+++ ExerciseDateTime/ExerciseDateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your Current time is: " + DateTime.Now);
+            Console.WriteLine("Please enter a number.");
+            double getNum = Convert.ToDouble(Console.ReadLine());
+            DateTime newDate = DateTime.Now;
+            Console.WriteLine("In {0} hours it will be {1}",getNum, newDate.AddHours(getNum));
+
+            Console.ReadLine();
+        }
+    }
+}
