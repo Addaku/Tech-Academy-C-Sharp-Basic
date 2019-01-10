25013abcb34cfc4c3fbf3dfe162a23328ff81d10
Adrean <44714401+Addaku@users.noreply.github.com>
2019-01-10 14:17:10 -0800

    Add files via upload

---
+++ AdditionalFeatures/AdditionalFeatures/Bank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    class Bank
+    {
+
+        public Bank(string name) : this(name, 1000) { }
+        public Bank(string name, int chosenNumber)
+        {
+            Balance = chosenNumber;
+            User = name;
+        }
+        public int Balance { get; set; }
+        public string User { get; set; }
+
+    }
+}
---
+++ AdditionalFeatures/AdditionalFeatures/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+            const string name = "Adrean";
+            Console.WriteLine("Hello, my name is {0}",name);
+            //2
+            var myNum = 14;
+            Console.WriteLine("My favorite number is {0}", myNum);
+            //3
+            Console.WriteLine("Please enter a name.");
+            string getName = Console.ReadLine();
+            Console.WriteLine("Please enter a number over 100.");
+            int getNum = Convert.ToInt32(Console.ReadLine());
+            Bank bank = new Bank(getName, getNum);
+            Console.WriteLine("{0}'s account has ${1} in it.", bank.User, bank.Balance);
+
+            Console.ReadLine();
+        }
+    }
+}
